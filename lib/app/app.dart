21d9cfcb6bdf55e1ac1d091ab9bdb37be1131e00
Yczar <babalolagbogo@gmail.com>
2024-02-15 04:55:10 +0100

    chore: create app

---
+++ lib/app/app.dart
@@ -0,0 +1,22 @@
+import 'package:flash_briefs/app/ui/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flash Briefs',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
+        useMaterial3: true,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
