d2196d09204ca652d633a1fb3b9750135c713f90
Yczar <babalolagbogo@gmail.com>
2024-02-15 04:56:07 +0100

    chore: create news summary screen

---
+++ lib/app/ui/screens/news_summary_screen.dart
@@ -0,0 +1,32 @@
+// import 'package:flutter/material.dart';
+// import 'your_model_imports_here.dart';
+
+// class NewsSummaryScreen extends StatelessWidget {
+//   final Post post;
+
+//   const NewsSummaryScreen({Key? key, required this.post}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(
+//           'Summary',
+//         ),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             Text(
+//               post.title ?? 'No Title',
+//               style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+//             ),
+//             // Display your summary content here
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
