15d53b64a5eac8ee0a82f48bee4a4f58a90d942f
Yczar <babalolagbogo@gmail.com>
2024-02-15 17:36:59 +0100

    chore: update news summary screen

--- lib/app/ui/screens/news_summary_screen.dart
+++ lib/app/ui/screens/news_summary_screen.dart
@@ -1,11 +1,9 @@
 import 'package:flash_briefs/app/data/models/news_article.dart';
 import 'package:flash_briefs/app/ui/cubits/summarize_article/summarize_article_cubit.dart';
 import 'package:flash_briefs/app/ui/cubits/summarize_article/summarize_article_state.dart';
+import 'package:flash_briefs/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:http/http.dart' as http;
-import 'package:html/parser.dart' as parser;
-import 'package:html/dom.dart' as dom;
 
 class NewsSummaryScreen extends StatefulWidget {
   final NewsArticle article;
@@ -38,124 +36,88 @@ class _NewsSummaryScreenState extends State<NewsSummaryScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              widget.article.title ?? 'No Title',
-              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 20),
-            Expanded(
-              child: BlocProvider.value(
-                value: _summarizeArticleCubit,
-                child:
-                    BlocBuilder<SummarizeArticleCubit, SummarizeArticleState>(
-                  builder: (context, state) {
-                    return state.when(
-                      initial: () => const SizedBox.shrink(),
-                      loadInProgress: () => const Center(
-                        child: CircularProgressIndicator(strokeWidth: 2),
-                      ),
-                      loadSuccess: (summaries) => ListView.builder(
-                        itemCount: summaries.length,
-                        itemBuilder: (_, index) => RichText(
-                          text: TextSpan(
-                            style: const TextStyle(
-                                fontSize: 16.0,
-                                color: Colors.black), // Default text style
-                            children: parseMarkdown(
-                              summaries[index],
+        child: RefreshIndicator(
+          onRefresh: () => _summarizeArticleCubit.summarizeArticle(
+            widget.article.url ?? '',
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                widget.article.title ?? 'No Title',
+                style:
+                    const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: BlocProvider.value(
+                  value: _summarizeArticleCubit,
+                  child:
+                      BlocBuilder<SummarizeArticleCubit, SummarizeArticleState>(
+                    builder: (context, state) {
+                      return state.when(
+                        initial: () => const SizedBox.shrink(),
+                        loadInProgress: () => const Center(
+                          child: IntrinsicHeight(
+                            child: Column(
+                              children: [
+                                SizedBox(
+                                  height: 18,
+                                  width: 18,
+                                  child: CircularProgressIndicator(
+                                    strokeWidth: 2,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  '...Generating summary',
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                    color: Colors.black,
+                                  ),
+                                )
+                              ],
                             ),
                           ),
                         ),
-                        //  Text(
-                        //   summaries[index],
-                        //   style: GoogleFonts.openSans(
-                        //     fontSize: 16,
-                        //     color: Colors.black,
-                        //   ),
-                        // ),
-                      ),
-                      loadFailure: () => const Center(
-                        child: Text('Failed to fetch summary'),
-                      ),
-                    );
-                  },
+                        loadSuccess: (summaries) => ListView.builder(
+                          itemCount: summaries.length,
+                          itemBuilder: (_, index) => RichText(
+                            text: TextSpan(
+                              style: const TextStyle(
+                                fontSize: 16.0,
+                                color: Colors.black,
+                              ), // Default text style
+                              children: Utils.parseMarkdown(
+                                summaries[index],
+                              ),
+                            ),
+                          ),
+                        ),
+                        loadFailure: () => const Center(
+                          child: Text('Failed to fetch summary'),
+                        ),
+                      );
+                    },
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 
   Future<void> _summarizeArticle() async {
-    final webContent =
-        await _extractAllParagraphsFromUrl(widget.article.url ?? '');
-
-    _summarizeArticleCubit.summarizeArticle(webContent);
-  }
-
-  Future<String> _extractAllParagraphsFromUrl(String url) async {
-    final response = await http.get(Uri.parse(url));
-
-    if (response.statusCode == 200) {
-      // Parse the HTML document
-      dom.Document document = parser.parse(response.body);
-
-      // Extract all paragraph elements
-      List<dom.Element> paragraphElements = document.querySelectorAll('p');
-      String allParagraphsText =
-          paragraphElements.map((e) => e.text.trim()).join('\n\n');
-
-      return allParagraphsText;
-    } else {
-      throw Exception('Failed to load webpage');
-    }
+    _summarizeArticleCubit.summarizeArticle(widget.article.url ?? '');
   }
 
   @override
   void dispose() {
     _summarizeArticleCubit.close();
     super.dispose();
-  }
-
-  List<TextSpan> parseMarkdown(String text) {
-    List<TextSpan> spans = [];
-    RegExp exp = RegExp(r'\*\*(.*?)\*\*');
-    String tempText = text;
-
-    while (exp.hasMatch(tempText)) {
-      final match = exp.firstMatch(tempText);
-      if (match != null) {
-        final beforeText = tempText.substring(0, match.start);
-        final boldText = match.group(1);
-
-        if (beforeText.isNotEmpty) {
-          spans.add(TextSpan(text: beforeText));
-          spans.add(const TextSpan(text: '\n\n'));
-        }
-        if (boldText != null) {
-          spans.add(
-            TextSpan(
-              text: boldText,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          );
-        }
-
-        tempText = tempText.substring(match.end);
-      }
-    }
-
-    if (tempText.isNotEmpty) {
-      spans.add(TextSpan(text: tempText));
-    }
-
-    return spans;
   }
 }
