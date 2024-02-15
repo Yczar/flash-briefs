d144d8f2761aa6adc0f221b5dbb4928acd4dd3d9
Yczar <babalolagbogo@gmail.com>
2024-02-15 17:35:31 +0100

    chore: update post list widget

--- lib/app/ui/widgets/post_lists_widget.dart
+++ lib/app/ui/widgets/post_lists_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flash_briefs/app/data/models/news_article.dart';
+import 'package:flash_briefs/app/ui/cubits/top_headlines/top_headlines_cubit.dart';
 import 'package:flash_briefs/app/ui/widgets/article_card_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class PostListsWidget extends StatelessWidget {
   const PostListsWidget({
@@ -11,15 +13,21 @@ class PostListsWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: articles
-            .map(
-              (article) => ArticleCardWidget(
-                article: article,
-              ),
-            )
-            .toList(),
+    return RefreshIndicator(
+      color: Colors.blue,
+      onRefresh: () async {
+        context.read<TopHeadlinesCubit>().fetchTopHeadlines();
+      },
+      child: SingleChildScrollView(
+        child: Column(
+          children: articles
+              .map(
+                (article) => ArticleCardWidget(
+                  article: article,
+                ),
+              )
+              .toList(),
+        ),
       ),
     );
   }
