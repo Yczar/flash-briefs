4ef676c983914d3a44fb0d5bc46d360496755e6b
Yczar <babalolagbogo@gmail.com>
2024-02-15 04:55:57 +0100

    chore: create splash screem

---
+++ lib/app/ui/screens/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flash_briefs/app/ui/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _delaySplash();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Flash Briefs',
+          style: GoogleFonts.openSans().copyWith(
+            fontWeight: FontWeight.w600,
+            color: Colors.black,
+            fontSize: 24,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _delaySplash() {
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (_) => const HomeScreen(),
+        ),
+      );
+    });
+  }
+}
